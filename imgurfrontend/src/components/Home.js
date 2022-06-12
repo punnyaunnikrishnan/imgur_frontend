25102c1b4221af14212e3f4ea2deabc8d1957947
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-12 19:49:28 +0530

    Home.js

---
+++ imgurfrontend/src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import UserService from "../services/user.service";
+function Home() {
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    UserService.getPublicContent().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+        setContent(_content);
+      }
+    );
+  }, []);
+  return <div className="container">home</div>;
+}
+export default Home;
